7da9433f8959d2ee0608eea8e72bd2abb33be590
Yash sinha <84098535+Yashsinha220@users.noreply.github.com>
2023-06-14 03:47:54 +0530

    login and signup changes

--- src/scenes/loginpage/Form.jsx
+++ src/scenes/loginpage/Form.jsx
@@ -18,6 +18,7 @@ import { useDispatch } from "react-redux";
 import { setLogin } from "../../state";
 import Dropzone from "react-dropzone";
 import FlexBetween from "../../components/FlexBetween";
+import { Input } from "postcss";
 // import {DropS} from 'react-dropzone'
 
 const registerSchema = yup.object().shape({
@@ -36,8 +37,8 @@ const loginschema = yup.object().shape({
 });
 
 const intialvalueRegister = {
-  firstName: "",
-  lastName: "",
+  firstname: "",
+  lastname: "",
   email: "",
   password: "",
   location: "",
@@ -50,40 +51,234 @@ const intialvalueLogin = {
   password: "",
 };
 function Form() {
-  const { pagetype, setPageType } = useState("login");
+  const [pagetype, setPageType] = useState("register");
   const { palette } = useTheme();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const isNonMobile = useMediaQuery("(min-width:600px)");
   const islogin = pagetype === "login";
+  // const isRegister = ;
   const isRegister = pagetype === "register";
 
-  const hadleformsubmit = async (values, onSubmitProps) => {};
-  return;
-  <Formik
-    onSubmit={hadleformsubmit}
-    isInitialValid={islogin ? intialvalueLogin : intialvalueRegister}
-    validationSchema={islogin ? loginschema : registerSchema}
-  >
-    {({
-      values,
-      errors,
-      touched,
-      handleBlur,
-      handleChange,
-      handleSubmit,
-      setFieldvalue,
-      resetForm,
-    }) => (
-      <form onSubmit={handleSubmit}>
-        <Box
-          display={"grid"}
-          gap={"30px"}
-          gridTemplateColumns={"repeat(4 , minmax(0 , 1fr))"}
-        ></Box>
-      </form>
-    )}
-  </Formik>;
+  const register = async (values, onSubmitProps) => {
+    // we need to use the form data to store our form data
+    // allow us to send the form infor with image
+    const formData = new FormData();
+    for (let value in values) {
+      formData.append(value, values[value]);
+    }
+    formData.append("picturepath", values.picture.name);
+    console.log(formData)
+    // const savedUserresponse = await fetch(
+    //   "http://localhost:3001/auth/register",
+    //   {
+    //     method: "POST",
+    //     body: formData,
+    //   }
+    // );
+
+    // const savedUser = await savedUserresponse.json();
+    // // for reseting the form
+    // onSubmitProps.resetForm();
+    // if (savedUser) {
+    //   setPageType("login");
+    // }
+  };
+  const login = async (values, onSubmitProps) => {};
+
+  const hadleformsubmit = async (values, onSubmitProps) => {
+    if (islogin) await login(values, onSubmitProps);
+    if (isRegister) await register(values, onSubmitProps);
+  };
+  return (
+    <Formik
+      onSubmit={hadleformsubmit}
+      initialValues={islogin ? intialvalueLogin : intialvalueRegister}
+      validationSchema={islogin ? loginschema : registerSchema}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        setFieldValue,
+        resetForm,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <Box
+            display={"grid"}
+            gap={"30px"}
+            gridTemplateColumns={"repeat(4 , minmax(0 , 1fr))"}
+            sx={{
+              "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+            }}
+          >
+            {isRegister && (
+              <>
+                <TextField
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.firstName}
+                  name="firstName"
+                  error={
+                    Boolean(touched.firstName) && Boolean(errors.firstName)
+                  }
+                  helperText={touched.firstName && errors.firstName}
+                  sx={{
+                    gridColumn: "span 2",
+                  }}
+                />
+
+                <TextField
+                  label="Last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.lastName}
+                  name="lastName"
+                  error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                  helperText={touched.lastName && errors.lastName}
+                  sx={{
+                    gridColumn: "span 2",
+                  }}
+                />
+
+                <TextField
+                  label="Location"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.location}
+                  name="location"
+                  error={Boolean(touched.location) && Boolean(errors.location)}
+                  helperText={touched.location && errors.location}
+                  sx={{
+                    gridColumn: "span 4",
+                  }}
+                />
+
+                <TextField
+                  label="Occupation"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.occupation}
+                  name="occupation"
+                  error={
+                    Boolean(touched.occupation) && Boolean(errors.occupation)
+                  }
+                  helperText={touched.occupation && errors.occupation}
+                  sx={{
+                    gridColumn: "span 4",
+                  }}
+                />
+
+                {/* drop zone */}
+                <Box
+                  gridColumn={"span 4"}
+                  border={`1px solid ${palette.neutral.medium}`}
+                  borderRadius={"5px"}
+                  padding={"1rem"}
+                >
+                  <Dropzone
+                    acceptedFiles={".jpeg, .jpg, .png , .pdf"}
+                    multiple={false}
+                    onDrop={(acceptedFiles) => {
+                      console.log(acceptedFiles);
+                      setFieldValue("picture", acceptedFiles[0]);
+                    }}
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <Box
+                        {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                        padding={"1rem"}
+                      >
+                        <input {...getInputProps()}></input>
+                        {!values.picture ? (
+                          <p>Add Picture Here</p>
+                        ) : (
+                          <FlexBetween>
+                            <Typography>{values.picture.name}</Typography>
+                            <EditOutlined></EditOutlined>
+                          </FlexBetween>
+                        )}
+                      </Box>
+                    )}
+                  </Dropzone>
+                </Box>
+              </>
+            )}
+
+            <TextField
+              label="Emal"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+              name="email"
+              error={Boolean(touched.email) && Boolean(errors.email)}
+              helperText={touched.email && errors.email}
+              sx={{
+                gridColumn: "span 4",
+              }}
+            />
+
+            <TextField
+              label="Password"
+              type="password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              name="password"
+              error={Boolean(touched.password) && Boolean(errors.password)}
+              helperText={touched.password && errors.password}
+              sx={{
+                gridColumn: "span 4",
+              }}
+            />
+          </Box>
+
+          {/* Buttons */}
+          <Box>
+            <Button
+              fullWidth
+              type="submit"
+              sx={{
+                m: "2rem 0",
+                p: "1rem",
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                "&:hover": {
+                  color: palette.primary.main,
+                },
+              }}
+            >
+              {islogin ? "Login" : "Register"}
+            </Button>
+            <Typography
+              onClick={() => {
+                setPageType(islogin ? "register" : "login");
+                resetForm();
+              }}
+              sx={{
+                textDecoration: "underline",
+                color: palette.primary.main,
+                "&:hover": {
+                  cursor: "pointer",
+                  color: palette.primary.light,
+                },
+              }}
+            >
+              {islogin
+                ? "Don't have an account ? Sign up here ."
+                : "Already have an account ? Loign here"}
+            </Typography>
+          </Box>
+        </form>
+      )}
+    </Formik>
+  );
 }
 
 export default Form;
